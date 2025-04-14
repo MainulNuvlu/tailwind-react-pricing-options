238faaa373d698efe58c867343bb7f268c1fa4c9
MainulNuvlu <mainulnuvlu.alias@gmail.com>
2025-04-14 11:36:47 +0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import DaisyNav from '../Components/DaisyNav/DaisyNav'
+import NavBar from '../Components/NavBar/NavBar'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <header>
+        <NavBar></NavBar>
+        <DaisyNav></DaisyNav>
+      </header>
+
+
+    </>
+  )
+}
+
+export default App
