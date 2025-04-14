db7c171be15bd81c0f6a12a4320a2b5e2e1551e4
MainulNuvlu <mainulnuvlu.alias@gmail.com>
2025-04-14 15:56:00 +0600

    First Commit

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './link';
+import { Menu, X } from 'lucide-react';
 
 const navLinks = [
     {
@@ -31,12 +32,38 @@ const navLinks = [
 
 
 const NavBar = () => {
-    return (
-        <nav>
 
-            <ul className='flex'>
+
+    const [open, setOpen] = useState(false);
+
+    const links = navLinks.map(route => <Link key={route.id} route={route}> </Link>)
+
+
+    return (
+        <nav className='flex justify-between mx-10 mt-4'>
+
+            <span className='flex' onClick={() => setOpen(!open)}>
+                {open ?
+                    <X className='md:hidden'></X> :
+                    <Menu className='md:hidden'></Menu>}
+
+
+
+                <ul className={`md:hidden absolute duration-1000
+                    ${open ? 'top-8' : '-top-40'}
+                     bg-amber-200 text-black`}>
+                    {links}
+                </ul>
+
+
+
+                <h3 className='ml-4'>My NavBar</h3>
+            </span>
+
+
+            <ul className='hidden md:flex'>
                 {
-                    navLinks.map(route => <Link key={route.id} route={route}> </Link>)
+                    links
                 }
             </ul>
 
@@ -54,6 +81,7 @@ const NavBar = () => {
                 <li className='mr-10'><a href="/about">About</a></li>
                 <li className='mr-10'><a href="/blog">Blog</a></li>
             </ul> */}
+            <button>Sign In</button>
         </nav>
     );
 };
